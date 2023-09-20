e95f0c679336dadf484fc5ed0ed9bcf2f9ca986c
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-20 01:55:05 -0500

    Fix linter errors

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -9,7 +9,7 @@ function App() {
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route  path="/" element={<Greeting />} />
+          <Route path="/" element={<Greeting />} />
         </Routes>
       </BrowserRouter>
     </Provider>
